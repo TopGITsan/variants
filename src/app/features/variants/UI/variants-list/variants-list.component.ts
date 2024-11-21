100bb948e41fc8bffc1467195ca0fc4f0dffaac1
TopGitSan <c.toparcean@gmail.com>
2024-11-21 16:33:57 +0100

    feat(variants-list): :sparkles: use virtual scroll to display list items

--- src/app/features/variants/UI/variants-list/variants-list.component.ts
+++ src/app/features/variants/UI/variants-list/variants-list.component.ts
@@ -8,19 +8,21 @@ import {
 } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
 import { Variant } from 'src/app/store/variants.state';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   selector: 'app-variants-list',
   standalone: true,
   styleUrls: ['./variants-list.component.scss'],
   templateUrl: './variants-list.component.html',
-  imports: [MatListModule, NgIf, NgFor],
+  imports: [MatListModule, NgIf, ScrollingModule],
 })
 export class VariantsListComponent {
   @Input() data: Variant[] = [];
   @Output() select = new EventEmitter<Variant>();
 
   trackVariantBy(index: number, variant: Variant) {
-    return variant.id ?? index;
+    return variant.id ?? String(index);
   }
 }
